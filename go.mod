afebda7ce6b14b00a9bb6698c406d29967834d8b
Alex Vassiliou <avassiliou@zendesk.com>
2020-04-22 18:29:13 +1000

    connecting to postgres and retrieving numbers

---
+++ go.mod
@@ -0,0 +1,12 @@
+module phone-number-normalizer
+
+go 1.14
+
+require (
+	github.com/jackc/pgconn v1.5.0
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/joho/godotenv v1.3.0
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20200420201142-3c4aac89819a // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main/main.go
+++ main/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/jackc/pgx"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	conxStr := pgConfig()
+	conn, err := pgx.Connect(conxStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err2 := conn.Query("SELECT * FROM phone_numbers")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	for rows.Next() {
+		var n string
+		err := rows.Scan(nil, &n)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(n))
+	}
+}
+
+func loadEnvVarialble(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func pgConfig() pgx.ConnConfig {
+	var config pgx.ConnConfig
+	config.Host = loadEnvVarialble("HOST")
+	config.User = loadEnvVarialble("USER")
+	config.Password = loadEnvVarialble("PASSWORD")
+	config.Database = loadEnvVarialble("DBNAME")
+
+	return config
 }
